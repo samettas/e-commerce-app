39af315657c9962f7f47ff345a842ca921e056b3
Samet Taş <bmsamettas@gmail.com>
2023-11-11 13:59:15 +0300

    creating a login page

---
+++ frontend/src/app/common/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ValidDirective } from '../directives/valid.directive';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ValidDirective
+  ],
+  exports:[
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ValidDirective
+  ]
+})
+export class SharedModule { }

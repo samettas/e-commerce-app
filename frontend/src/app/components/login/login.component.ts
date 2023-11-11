e8f90cf2de0fbf514100dfa2e017f4f9a80ead72
Samet Taş <bmsamettas@gmail.com>
2023-11-11 15:05:45 +0300

    creating toastr and spinner service

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 import { SharedModule } from 'src/app/common/shared/shared.module';
 
 @Component({
@@ -10,6 +12,17 @@ import { SharedModule } from 'src/app/common/shared/shared.module';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(
+    private _toastr: ToastrService,
+    private _spinner: NgxSpinnerService
+  ){
+    this._spinner.show();
+    setTimeout(()=>{
+      this._spinner.hide();
+    },5000)
+
+  }
 
   login(form:NgForm){
     if(form.valid){

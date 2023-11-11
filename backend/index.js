9e334c35b36cf66111f51df73f86f7d9e581f1b0
Samet Taş <bmsamettas@gmail.com>
2023-11-11 17:38:30 +0300

    category api requests

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,9 @@ app.use(express.json());
 app.use(cors());
 
 const authRouter = require("./routers/auth.router");
+const categoryRouter = require("./routers/category.router");
 app.use("/api/auth",authRouter);
-
+app.use("/api/category",categoryRouter);
 
 connection();
 

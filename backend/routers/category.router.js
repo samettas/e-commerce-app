31ab26164bbbd712db2d3227b882be279439fd14
Samet Taş <bmsamettas@gmail.com>
2023-11-12 17:53:31 +0300

    service-model creation and api requests for the category

--- backend/routers/category.router.js
+++ backend/routers/category.router.js
@@ -1,51 +1,58 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 const Category = require("../models/category");
-const {v4:uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
-router.post("/add",async (req,res)=>{
-    try{
-        const {name} = req.body;
-        const category = new Category({
-            _id: uuidv4(),
-            name: name
-        });
+router.post("/add", async (req, res) => {
+    try {
+        const { name } = req.body;
 
-        await category.save();
-        res.json({message:"Kategori kaydı yapıldı!"});
-    }catch(error){
-        console.stats(500).json({message: error.message});
+        const checkName = await Category.findOne({ name: name });
+        if (checkName != null) {
+            res.status(403).json({ message: "Bu kategori daha önceden oluşturulmuş!" })
+        } else {
+            const category = new Category({
+                _id: uuidv4(),
+                name: name
+            });
+
+            await category.save();
+            res.json({ message: "Kategori kaydı başarıyla tamamlandı!" });
+        }
+
+    } catch (error) {
+        console.stats(500).json({ message: error.message });
     }
 });
 
-router.post("/removeById",async(req,res)=>{
-    try{
-        const {_id} = req.body;
+router.post("/removeById", async (req, res) => {
+    try {
+        const { _id } = req.body;
         await Category.findByIdAndRemove(_id);
-        res.json({message:"Kategori kaydı silindi!"});
-    }catch(error){
-        console.stats(500).json({message: error.message});
+        res.json({ message: "Kategori kaydı silindi!" });
+    } catch (error) {
+        console.stats(500).json({ message: error.message });
     }
 });
 
-router.post("/update",async(req,res)=>{
-    try{
-        const {_id,name} = req.body;
-        const category = await Category.findOne({_id:_id});
+router.post("/update", async (req, res) => {
+    try {
+        const { _id, name } = req.body;
+        const category = await Category.findOne({ _id: _id });
         category.name = name;
-        await Category.findByIdAndUpdate(_id,category);
-        res.json({message:"Kategori kaydı güncellendi!"});
-    }catch(error){
-        console.stats(500).json({message: error.message});
+        await Category.findByIdAndUpdate(_id, category);
+        res.json({ message: "Kategori kaydı güncellendi!" });
+    } catch (error) {
+        console.stats(500).json({ message: error.message });
     }
 });
 
-router.get("/getAll",async(req,res)=>{
-    try{
-        const categories = await Category.find().sort({name:1});
+router.get("/", async (req, res) => {
+    try {
+        const categories = await Category.find().sort({ name: 1 });
         res.json(categories);
-    }catch(error){
-        console.stats(500).json({message: error.message});
+    } catch (error) {
+        console.stats(500).json({ message: error.message });
     }
 });
 

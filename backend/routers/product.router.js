58cf82a99377e5a3a6ec91dffd5049a87108b605
Samet Taş <bmsamettas@gmail.com>
2023-11-13 12:18:26 +0300

    product deletion and listing

--- backend/routers/product.router.js
+++ backend/routers/product.router.js
@@ -27,3 +27,56 @@ router.post("/add", upload.array("images"), async(req,res)=>{
     });
 
 });
+
+//Ürün Silme
+router.post("/removeById" , async(req,res)=>{
+    response(res, async()=>{
+        const {_id}= req.body;
+
+        const product = await Product.findById(_id);
+        for(const image.path, ()=>{}){
+            fs.unlink(image.path, ()=>{});
+        }
+
+        await Product.findByIdAndRemove(_id);
+        res.json({message: "Ürün kaydı başarıyla silindi!"})
+    });
+});
+
+//Ürün Listeleme
+router.post("/", async(req, res)=>{
+    response(res, async()=>{
+        const {pageNumber, pageSize, search} = req.body;
+
+        let productCount = await Product.find({
+            $or: [
+                {
+                    name:{ $regex: search, $options:"i"}
+                }
+            ]
+        }).count();
+
+        let products = await Product.find({
+            $or: [
+                {
+                    name:{ $regex: search, $options:"i"}
+                }
+            ]
+        }).sort({name: 1})
+        .populate("categories")
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize);
+
+        let totalPageCount = Math.ceil(productCount / pageSize);
+        let model = {
+            datas: products,
+            pageNumber: pageNumber,
+            pageSize: pageSize,
+            totalPageCount: totalPageCount,
+            isFirstPage: pageNumber == 1 ? true : false,
+            isLastPAge: totalPageCount == pageNumber ? true : false
+        };
+
+        res.json(model);
+    });
+});

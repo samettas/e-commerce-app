bdd213ad7722448974e24e89d33e4865f11f7dd8
Samet Taş <bmsamettas@gmail.com>
2023-11-14 16:05:56 +0300

    home component design

--- frontend/src/app/common/models/request.model.ts
+++ frontend/src/app/common/models/request.model.ts
@@ -2,4 +2,6 @@ export class RequestModel{
     pageNumber: number = 1;
     pageSize: number = 10;
     search: string = "";
+    categoryName: string = "Tümü";
+    priceFilter: string = "0";
 }
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from 'src/app/common/shared/shared.module';
+import { CategoryModel } from '../categories/models/category.model';
+import { CategoryService } from '../categories/services/category.service';
+import { RequestModel } from 'src/app/common/models/request.model';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +11,26 @@ import { SharedModule } from 'src/app/common/shared/shared.module';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  categories: CategoryModel[] = [];
+  request: RequestModel = new RequestModel();
 
+  constructor(
+    private _category: CategoryService
+  ) { }
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getAll(){
+    
+  }
+
+  getCategories() {
+    this._category.getAll(res => this.categories = res);
+  }
+
+  changeCategory(categoryName:string){
+    this.request.categoryName = categoryName;
+  }
 }
